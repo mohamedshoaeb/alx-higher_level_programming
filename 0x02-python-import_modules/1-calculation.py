6a6b6e7a029cc3df2f3a0faec0f7e62c7fdc4642
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-03-09 14:25:54 +0000

    Some Tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	"""print sum , sub , avg and mul"""
-	import calculator_1 as model
-
-	a = 10
-	b = 5
-
-	print("{} + {} = {}".format(a , b , model.add(a , b)))
-	print("{} - {} = {}".format(a, b, model.sub(a, b)))
-	print("{} * {} = {}".format(a, b, model.mul(a, b)))
-	print("{} / {} = {}".format(a, b, model.div(a, b)))
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
